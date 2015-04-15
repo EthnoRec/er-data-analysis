55481b406cf1e6c6fcb387a64e1b0de7e5d35425
Lee Archer <la12808@my.bristol.ac.uk>
2015-04-15 19:28:06 +0100

    Refactoring.

--- analytics.py
+++ analytics.py
@@ -34,58 +34,54 @@ class AvgFace:
         M[:,2] += midpoint(self.avg_eye_left,self.avg_eye_right) - midpoint(face.eye_left,face.eye_right)
 
         m = face.imread()
-        m2 = cv2.warpAffine(m,M,(540,540))
+        m2 = cv2.warpAffine(m,M,(128,128))
         #cv2.line(m2,tuple(self.avg_eye_left),tuple(self.avg_eye_right),(200,0,0),2)
         return m2
     def __avg_eyes(self):
-        def midpoints(boxes):
-            xs = 0.5*(boxes[:,2] + boxes[:,0])
-            ys = 0.5*(boxes[:,3] + boxes[:,1])
-            return np.uint16([xs,ys]).transpose()
-
         with psycopg2.connect(con_str) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-                cur.execute("SELECT min(origin_x) AS origin_x, min(origin_y) \
-                        AS origin_y,max(extent_x) AS extent_x, max(extent_y) AS extent_y, \
-                        fd.image_id AS image_id \
-                        FROM \"Boxes\" AS bs JOIN \"FaceDetections\" as fd \
-                        ON fd._id = bs.fd_id WHERE part_index between 9 and 14 AND fd.component = 6 AND score > 0.7 \
-                        GROUP BY fd._id,fd.image_id;")
-                eye_left_rs = cur.fetchall()
-                eye_left_data = np.array(eye_left_rs)
+                query = """
+                        SELECT * FROM detection_eyes
+                        JOIN \"People\" AS p ON p._id = person_id
+                        WHERE component = 6 AND score > 0.5 AND
+                        p.origin_lat = 20.659699;
+                        """
+                cur.execute(query)
+                res = cur.fetchall()
 
-                # Midpoints of left-eye boxes (merged by PostgreSQL)
-                left_eyes = midpoints(np.uint16(eye_left_data[:,:-1]))
+                self.faces = []
+                for row in res:
+                    eye_left_m = np.mean([[row["left_origin_x"],row["left_origin_y"]],[row["left_extent_x"],row["left_extent_y"]]],axis=0)
+                    eye_right_m = np.mean([[row["right_origin_x"],row["right_origin_y"]],[row["right_extent_x"],row["right_extent_y"]]],axis=0)
+                    self.faces.append(Face(image_id=row["image_id"],eye_left=eye_left_m,eye_right=eye_right_m))
+                self.avg_eye_left = np.array([64-12,64])
+                self.avg_eye_right = np.array([64+12,64])
 
-                image_ids = eye_left_data[:,-1]
-
-                # Average of these midpoints
-                self.avg_eye_left = np.uint16(left_eyes.mean(axis=0))
-
-                cur.execute("SELECT min(origin_x) as origin_x, min(origin_y) \
-                        AS origin_y,max(extent_x) AS extent_x, max(extent_y) AS extent_y \
-                        FROM \"Boxes\" AS bs JOIN \"FaceDetections\" as fd \
-                        ON fd._id = bs.fd_id WHERE part_index between 20 and 25 AND fd.component = 6 AND score > 0.7 \
-                        GROUP BY fd._id,fd.image_id;")
-                eye_right_rs = cur.fetchall()
-
-                # Midpoints of right-eye boxes (merged by PostgreSQL)
-                right_eyes = midpoints(np.array(eye_right_rs))
-
-                self.avg_eye_right = np.uint16(right_eyes.mean(axis=0))
-                self.faces = [Face(eye_left=eye_left,eye_right=eye_right,image_id=image_id) for eye_left,eye_right,image_id in zip(left_eyes,right_eyes,image_ids)]
-
-def main():
-
-
+def gen_avg():
     af = AvgFace()
-    total = np.zeros((540,540,3))
-    for face in af.faces:
+    total = np.zeros((128,128,3))
+    total_score = 0
+    n = len(af.faces)
+    print("Total faces: ",n)
+    for face in af.faces[:n]:
         fe = af.fit_eyes(face)
         total += fe
-        #cv2.imwrite("mapped/m_{}.jpg".format(face.image_id),fe)
-    avg = total/len(af.faces)
+        cv2.imwrite("mapped/m_{}.jpg".format(face.image_id),fe)
+    avg = total/n
     cv2.imwrite("avg.jpg",avg)
+
+
+def main():
+    gen_avg()
+    #from sklearn.decomposition import IncrementalPCA
+    #af = AvgFace()
+    #faces = [np.ravel(af.fit_eyes(face)) for face in af.faces]
+    ##print(len(faces))
+    #n_components = 74
+    #ipca = IncrementalPCA(n_components=n_components, batch_size=None)
+    #ipca.fit(faces)
+    #import pdb; pdb.set_trace()
+    
 
 
 
